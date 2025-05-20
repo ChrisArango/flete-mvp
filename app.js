bfd4ea0d8c9461854421a89a846645833124b4c8
Chris Arango <christianarango90nlc@gmail.com>
2025-05-19 21:13:52 -0500

    Actualización y limpieza codigo en archivos

--- app.js
+++ app.js
@@ -1,55 +1,12 @@
-//configura la app de Express (rutas, middlewares, etc.)
-
-// -----------------------
-/*
-importa Express,para crear:
-- El servidor.
-- manejar rutas
-- peticiones HTTP
--middlewares, etc.
-*/
+// ver informacion detallada en proyecto flet-notion
 const express = require('express');
 
-
-// -------------------------------------------
-/*
-Crea una instancia de la aplicación Express.
-A partir de aquí, app es el servidor en construcción.
-Con él puedes:
-- Definir rutas (app.get(), app.post(), etc.)
-- Agregar middlewares (app.use())
-- Manejar errores
-*/
 const app = express();
 
-
-// ------------------------------------------------
-/*
-Esto le dice a Express:
-Acepta y convierte automáticamente el cuerpo de las peticiones en formato JSON.
-Sin esta línea, Express no sabría interpretar ese JSON.
-*/
 app.use(express.json());
 
-
-// -------------------------------------------------
-/*
-Ruta de prueba:(ejemplo temporal)
-Dice:
-“Cuando el navegador/cliente visite la ruta raíz "/ ".responde con ¡Hola desde app.js!”.
-- request: Es la petición del cliente/navegador.
-- response: Es la respuesta del servidor
-- res.send(): Envia un texto de vuelta al cliente
-*/
 app.get('/', (req, res) => {
     res.send('¡Servidor funcionando correctamente!');
 });
 
-
-// -----------------------------------------------
-/*
-Esto es clave:
-- permite exportar la variable "app" para que server.js pueda importarla y usarla.
-- Así separas la lógica de configuración (app.js) de la lógica que arranca el servidor (server.js).
-*/
 module.exports = app;
--- server.js
+++ server.js
@@ -1,49 +1,11 @@
-/*  
-Solo se encarga de iniciar el servidor y leer variables de entorno.
-Esto es importante porque así no tienes que escribir valores sensibles directamente en el código. 
-Esto es una buena práctica en cualquier proyecto profesional o de portafolio.
-*/
+// ver informacion detallada en proyecto flet-notion
 
-// ---------------------------------
-/*
-require('dotenv'): Esto importa el paquete dotenv,Trae las funciones de esta librería a mi archivo.
-.config(): Llama al método config() de dotenv, que lo que hace es:
-- Leer el archivo .env
-- Cargar las variables que contiene en process.env
-
-útil porque separa las configuraciones del código. 
-Así puedes cambiar el puerto, credenciales, claves API, etc., sin tocar el código fuente.
-*/
 require('dotenv').config();
 
-
-// ----------------------------------------
-/*
-Importa la instancia de Express que se creo en (app.js).
-Así puedes usar el metodo app.listen() para iniciar el servidor.
-'./app' hace referencia al archivo (app.js) que ya se configuro.
-*/
 const app = require('./app');
 
-
-// ------------------------------------------
-/*
-Define el puerto en el que se ejecutará tu servidor.
-Si tienes una variable PORT en el archivo (.env), usará ese valor.
-Si no, por defecto usará el puerto 3000.
-
-Es una buena práctica dejar un puerto por defecto en caso de que no haya configuración.
-*/
 const PORT = process.env.PORT || 3000;
 
-
-// --------------------------------------
-/*
-Inicia el servidor Express para que empiece a escuchar conexiones en el puerto definido.
-- PORT: el puerto donde se escucharán las solicitudes (ejemplo: http://localhost:3000)
-- (()=>{...}): Función como segundo argumento que se ejecuta una vez que el servidor arranca correctamente.
-- Imprime un mensaje en la consola para que sepas que el servidor ya está funcionando, y en qué puerto
-*/
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
