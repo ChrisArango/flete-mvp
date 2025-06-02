4f0e9ee1e8f43fe6aaf138ac208f99fc766af15a
Chris Arango <christianarango90nlc@gmail.com>
2025-06-02 12:24:41 -0500

    actualizacion de archivo authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,22 +1,18 @@
 const User = require("../models/user");
-let  usuarios =[]; // base de datos teporal
+let  usuarios =[];
 
 const registerUser = (req, res) => {
-  // Extrae los datos enviados por el cliente (nombre, email y password).
+
   const {nombre, email, password} = req.body;
 
-  // Valida que estén completos.
   if (!nombre || !email || !password) {
     return res.status(404).json({mensaje: "Todos los campos son obligatorios."});
   }
 
-  // Crea un nuevo usuario con el modelo.
   const nuevoUsusario = new User (nombre, email, password);
 
-  // Lo guarda temporalmente en un array.
   usuarios.push(nuevoUsusario);
 
-  // Responde con un mensaje de éxito y los datos.
   res.status(201).json({mensaje: "Usuario registrado con éxito", usuario: nuevoUsusario});
 };
 
